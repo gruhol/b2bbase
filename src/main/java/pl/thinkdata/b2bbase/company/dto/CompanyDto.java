e6445e7cb3bc44cab3091b71bc9e0923a115f9d1
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2023-05-20 13:22:07 +0200

    Add tests to NipNumberIsTakenPredicate.

--- src/main/java/pl/thinkdata/b2bbase/company/dto/CompanyDto.java
+++ src/main/java/pl/thinkdata/b2bbase/company/dto/CompanyDto.java
@@ -3,11 +3,13 @@ package pl.thinkdata.b2bbase.company.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.pl.NIP;
 import pl.thinkdata.b2bbase.company.model.ComapnyType;
 import pl.thinkdata.b2bbase.company.model.LegalForm;
 
 @Getter
+@Setter
 public class CompanyDto {
     @NotBlank
     private String name;
---
+++ src/test/java/pl/thinkdata/b2bbase/company/validator/predicate/NipNumberIsTakenPredicateTest.java
@@ -0,0 +1,48 @@
+package pl.thinkdata.b2bbase.company.validator.predicate;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.thinkdata.b2bbase.company.dto.CompanyDto;
+import pl.thinkdata.b2bbase.company.service.CompanyService;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class NipNumberIsTakenPredicateTest {
+
+    @InjectMocks
+    NipNumberIsTakenPredicate predicate;
+    @Mock
+    CompanyService companyService;
+
+
+    @Test
+    public void shouldReturnTrueIfNipIsTaken() {
+        //given
+        CompanyDto companyDto = new CompanyDto();
+        companyDto.setNip("1234567890");
+        //when
+        when(companyService.findByNip(companyDto.getNip())).thenReturn(Boolean.TRUE);
+        boolean result = predicate.test(companyDto);
+        //then
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldReturnFalseIfNipIsNotTaken() {
+        //given
+        CompanyDto companyDto = new CompanyDto();
+        companyDto.setNip("1234567890");
+        //when
+        when(companyService.findByNip(companyDto.getNip())).thenReturn(Boolean.FALSE);
+        boolean result = predicate.test(companyDto);
+        //then
+        assertFalse(result);
+    }
+
+}
