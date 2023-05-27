925658a67fb9e1db97725ded1a8b652585f83adc
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2023-05-27 21:02:35 +0200

    Add spring security and token.

---
+++ src/main/java/pl/thinkdata/b2bbase/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package pl.thinkdata.b2bbase.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    private String secret;
+
+    public SecurityConfig(@Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           AuthenticationManager authenticationManager,
+                                           UserDetailsService userDetailsService) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/company/**").authenticated()
+                .anyRequest().permitAll()
+        );
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilter(new JwtAuthorizationFilter(authenticationManager, userDetailsService, secret));
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration ac) throws Exception {
+        return ac.getAuthenticationManager();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("test")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+}
