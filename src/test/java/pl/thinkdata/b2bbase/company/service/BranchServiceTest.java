0b0f38ccd14b920038f91135133e65e6aaf7c21e
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2023-11-11 17:14:38 +0100

    Add BranchServiceTest and CategoryServiceTest.

--- src/test/java/pl/thinkdata/b2bbase/company/service/BranchServiceTest.java
+++ src/test/java/pl/thinkdata/b2bbase/company/service/BranchServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import pl.thinkdata.b2bbase.common.error.InvalidRequestDataException;
 import pl.thinkdata.b2bbase.common.util.MessageGenerator;
 import pl.thinkdata.b2bbase.company.dto.BranchDto;
+import pl.thinkdata.b2bbase.company.dto.BranchToEditDto;
 import pl.thinkdata.b2bbase.company.model.Branch;
 import pl.thinkdata.b2bbase.company.model.Company;
 import pl.thinkdata.b2bbase.company.repository.BranchRepository;
@@ -19,7 +20,7 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BranchServiceTest {
@@ -56,6 +57,62 @@ class BranchServiceTest {
         //then
         InvalidRequestDataException exception = assertThrows(InvalidRequestDataException.class,
                 () -> branchService.addBranch(branchDto,  any()));
+        assertEquals("Możesz wprowadzić tylko jedną siedzibę główną", exception.getMessage());
+    }
+
+    @Test
+    void shouldSaveBranch() {
+        //given
+        BranchDto branchDto = new BranchDto();
+        branchDto.setName("Some Company");
+        branchDto.setHeadquarter(false);
+        Branch branchInDataBase = Branch.builder().headquarter(true).build();
+        Company company = Company.builder().branches(Arrays.asList(branchInDataBase)).build();
+        //when
+        when(companyService.getCompany(any())).thenReturn(company);
+        //then
+        branchService.addBranch(branchDto, any());
+        verify(branchRepository, atLeastOnce()).save(any());
+    }
+
+    @Test()
+    void shouldThrowInvalidRequestDataExceptionWhenEditBranchThatIsNotOwnBranch() {
+        BranchToEditDto branchToEditDto = new BranchToEditDto();
+        branchToEditDto.setId(13L);
+        Branch branchInDataBase = Branch.builder()
+                .id(12L)
+                .build();
+        Company company = Company.builder()
+                .branches(Arrays.asList(branchInDataBase))
+                .build();
+        //when
+        when(companyService.getCompany(any())).thenReturn(company);
+        when(branchRepository.findAllByCompany(any())).thenReturn(Arrays.asList(branchInDataBase));
+        //then
+        InvalidRequestDataException exception = assertThrows(InvalidRequestDataException.class,
+                () -> branchService.editBranch(branchToEditDto,  any()));
+        assertEquals("Niepoprawne dane oddziału", exception.getMessage());
+    }
+
+    @Test()
+    void shouldThrowInvalidRequestDataExceptionWhenEditHeadquartersBranchWhenOneAlreadyExist() {
+        BranchToEditDto branchToEditDto = new BranchToEditDto();
+        branchToEditDto.setId(13L);
+        branchToEditDto.setName("Some Company");
+        branchToEditDto.setHeadquarter(true);
+        Branch branchInDataBase = Branch.builder()
+                .id(13L)
+                .headquarter(true)
+                .build();
+        Company company = Company.builder()
+                .branches(Arrays.asList(branchInDataBase))
+                .build();
+        //when
+        when(companyService.getCompany(any())).thenReturn(company);
+        when(branchRepository.findAllByCompany(any())).thenReturn(Arrays.asList(branchInDataBase));
+        //then
+        InvalidRequestDataException exception = assertThrows(InvalidRequestDataException.class,
+                () -> branchService.editBranch(branchToEditDto,  any()));
         assertEquals("Możesz wprowadzić tylko jedną siedzibę główną", exception.getMessage());
     }
 
---
+++ src/test/java/pl/thinkdata/b2bbase/company/service/CategoryServiceTest.java
@@ -0,0 +1,123 @@
+package pl.thinkdata.b2bbase.company.service;
+
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletRequest;
+import pl.thinkdata.b2bbase.company.dto.CategoryResponse;
+import pl.thinkdata.b2bbase.company.model.Category;
+import pl.thinkdata.b2bbase.company.model.Company;
+import pl.thinkdata.b2bbase.company.repository.CategoryRepository;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceTest {
+
+    @Mock
+    private CategoryRepository categoryRepository;
+    @Mock
+    private CompanyService companyService;
+    @Inject
+    private CategoryService categoryService;
+
+    @BeforeEach
+    void init() {
+        this.categoryService = new CategoryService(this.categoryRepository, this.companyService);
+    }
+
+    @Test
+    void shouldReturnCompanyCategorySelected() {
+        //when
+        when(categoryRepository.findAll()).thenReturn(getAllCategory());
+        when(companyService.getCompany(any())).thenReturn(getCompanyCategory());
+        //then
+        List<CategoryResponse> response = categoryService.getCategories(new MockHttpServletRequest());
+        long countSelected = response.stream()
+                .flatMap(cat -> cat.getChildren().stream())
+                .map(catSelected -> catSelected.getSelected() == true)
+                .filter(istrue -> istrue == true).count();
+        assertEquals(2, response.size());
+        assertEquals(2, countSelected);
+    }
+
+    private List<Category> getAllCategory() {
+        Category category1 = Category.builder()
+                .id(1L)
+                .name("Category1")
+                .slug("category1")
+                .build();
+        Category category2 = Category.builder()
+                .id(2L)
+                .name("Category2")
+                .parent(category1)
+                .slug("category2")
+                .build();
+        Category category3 = Category.builder()
+                .id(3L)
+                .name("Category3")
+                .parent(category1)
+                .slug("category3")
+                .build();
+        Category category4 = Category.builder()
+                .id(4L)
+                .name("Category4")
+                .slug("category4")
+                .build();
+        Category category5 = Category.builder()
+                .id(5L)
+                .name("Category5")
+                .parent(category4)
+                .slug("category5")
+                .build();
+        return Arrays.asList(category1, category2, category3, category4, category5);
+    }
+
+    private Company getCompanyCategory() {
+        Category category1 = Category.builder()
+                .id(1L)
+                .name("Category1")
+                .slug("category1")
+                .build();
+        Category category2 = Category.builder()
+                .id(2L)
+                .name("Category2")
+                .parent(category1)
+                .slug("category2")
+                .build();
+        Category category3 = Category.builder()
+                .id(3L)
+                .name("Category3")
+                .parent(category1)
+                .slug("category3")
+                .build();
+        Category category4 = Category.builder()
+                .id(4L)
+                .name("Category4")
+                .slug("category4")
+                .build();
+        Category category5 = Category.builder()
+                .id(5L)
+                .name("Category5")
+                .parent(category4)
+                .slug("category5")
+                .build();
+        Set<Category> setCategory = new HashSet<>();
+        setCategory.add(category5);
+        setCategory.add(category2);
+        Company company = Company.builder()
+                .categories(setCategory)
+                .build();
+        return company;
+    }
+}
