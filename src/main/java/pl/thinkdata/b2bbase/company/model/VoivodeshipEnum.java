a88d32b9130977dbc948e0ffdd8b3296bd05766f
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2023-11-23 00:42:05 +0100

    Add slug to VoivodeshipEnum.

--- src/main/java/pl/thinkdata/b2bbase/company/model/VoivodeshipEnum.java
+++ src/main/java/pl/thinkdata/b2bbase/company/model/VoivodeshipEnum.java
@@ -1,26 +1,36 @@
 package pl.thinkdata.b2bbase.company.model;
 
 public enum VoivodeshipEnum {
-    DS("dolnośląskie"),
-    KP("kujawsko-pomorskie"),
-    LU("lubelskie"),
-    LB("lubuskie"),
-    LD("łódzkie"),
-    MA("małopolskie"),
-    MZ("mazowieckie"),
-    OP("opolskie"),
-    PK("podkarpackie"),
-    PD("podlaskie"),
-    PM("pomorskie"),
-    SL("śląskie"),
-    SK("świętokrzyskie"),
-    WN("warmińsko-mazurskie"),
-    WP("wielkopolskie"),
-    ZP("zachodniopomorskie");
+    DS("dolnośląskie", "dolnoslaskie"),
+    KP("kujawsko-pomorskie", "kujawsko-pomorskie"),
+    LU("lubelskie", "lubelskie"),
+    LB("lubuskie", "lubuskie"),
+    LD("łódzkie", "lodzkie"),
+    MA("małopolskie", "malopolskie"),
+    MZ("mazowieckie", "mazowieckie"),
+    OP("opolskie", "opolskie"),
+    PK("podkarpackie", "podkarpackie"),
+    PD("podlaskie", "podlaskie"),
+    PM("pomorskie", "pomorskie"),
+    SL("śląskie", "slaskie"),
+    SK("świętokrzyskie", "swietokrzyskie"),
+    WN("warmińsko-mazurskie", "warminsko-mazurskie"),
+    WP("wielkopolskie", "wielkopolskie"),
+    ZP("zachodniopomorskie", "zachodniopomorskie");
 
     private String value;
+    private String slug;
 
-    VoivodeshipEnum(String value) {
+    VoivodeshipEnum(String value, String slug) {
         this.value = value;
+        this.slug = slug;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getSlug() {
+        return slug;
     }
 }
