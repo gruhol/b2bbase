520efc50ae04c5dc4175e62ef5fecb7347ca6351
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2024-02-28 09:44:38 +0100

    Add sql to new table.

---
+++ src/main/resources/testdatabase/2024-02-28/01-create-static-page.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset dabrowskiw:13
+CREATE TABLE static_page(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    added_date DATETIME NOT NULL,
+    edit_date DATETIME NOT NULL,
+    content TEXT,
+    slug VARCHAR(128) NOT NULL UNIQUE
+);
