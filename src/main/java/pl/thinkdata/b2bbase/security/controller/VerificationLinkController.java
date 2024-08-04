cfa5bcfafad01e9473728334c9032bb760b59bca
gruhol <dabrowskiw@gmail.com>
2024-08-04 13:12:23 +0200

    Add token to verificationlink checker.

--- src/main/java/pl/thinkdata/b2bbase/security/controller/VerificationLinkController.java
+++ src/main/java/pl/thinkdata/b2bbase/security/controller/VerificationLinkController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import pl.thinkdata.b2bbase.security.dto.VerificationLinkResponse;
 import pl.thinkdata.b2bbase.security.service.VerificationLinkService;
 
 @RestController
@@ -13,7 +14,7 @@ public class VerificationLinkController {
     private final VerificationLinkService service;
 
     @GetMapping("/verify/{token}")
-    public boolean checkVerificationLink(@PathVariable(value = "token", required = false) String token) {
-        return service.checkVerificationLink(token);
+    public VerificationLinkResponse checkVerificationLink(@PathVariable(value = "token", required = false) String token) {
+        return service.checkVerificationLinkAndSendToken(token);
     }
 }
