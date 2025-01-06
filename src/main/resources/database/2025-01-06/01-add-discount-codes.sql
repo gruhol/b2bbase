911973be95b378b27a79e32998c4faab228ef5d2
gruhol <dabrowskiw@gmail.com>
2025-01-06 23:23:45 +0100

    Add discount_codes table to database.

---
+++ src/main/resources/database/2025-01-06/01-add-discount-codes.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+--changeset dabrowskiw:20
+CREATE TABLE discount_codes (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    code VARCHAR(50) NOT NULL,
+    discount_type ENUM('percentage', 'value') NOT NULL,
+    discount_amount DECIMAL(10, 2) NOT NULL,
+    usage_limit INT DEFAULT 1,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at DATE NOT NULL
+);
